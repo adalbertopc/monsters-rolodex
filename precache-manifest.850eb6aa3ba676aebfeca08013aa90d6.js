a604cc784a729fb5e3186d16ebca4795094e597f
adalbertopc <58749813+adalbertopc@users.noreply.github.com>
2020-06-06 19:57:22 -0600

    Updates

---
+++ precache-manifest.850eb6aa3ba676aebfeca08013aa90d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "773a4c9df973b265820f9ee15a5e8fd5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d4f4026d4e7baf0a4d5",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "614c29f75c0619efca64",
+    "url": "/static/js/2.051a2960.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.051a2960.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d4f4026d4e7baf0a4d5",
+    "url": "/static/js/main.131b6289.chunk.js"
+  },
+  {
+    "revision": "4708b13b98379c58638d",
+    "url": "/static/js/runtime-main.03d56121.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
